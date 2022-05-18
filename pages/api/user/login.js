3879769747fd31f88160691d68930a09257fad3b
asagiman <lagavulin.jb.dc@gmail.com>
2022-05-19 00:03:23 +0900

    1

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -5,10 +5,17 @@ const loginUser = async (req, res) => {
     try {
         await connectDB()
         const savedUserData = await UserModel.findOne({email: req.body.email})
-        console.log(savedUserData)
-        return res.status(200).json({message: "ログイン成功"})
+
+        if(savedUserData){
+            if(req.body.password === savedUserData.password){
+            return res.status(200).json({message: "ログイン成功"})
+        } else {
+            return res.status(400).json({message: "ログイン失敗：パスワードが間違っています"})}
+        } else{ 
+            return res.status(400).json({message: "ログイン失敗：ユーザー登録をしてください"})
+        }
     } catch(err){
-        return res.status(400).json({message: "ログイン失敗"})
+            return res.status(400).json({message: "ログイン失敗"})
     }
 }
 
