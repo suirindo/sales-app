f000e149f4bd665244d73f0f3201f641d79d95dc
asagiman <lagavulin.jb.dc@gmail.com>
2022-05-19 23:08:57 +0900

    1

--- pages/api/item/update/[id].js
+++ pages/api/item/update/[id].js
@@ -3,6 +3,7 @@ import { ItemModel } from "../../../../utils/schemaModels";
 import auth from "../../../../utils/auth";
 
 const updateItem = async (req, res)  => {
+    console.log(req)
     try{
         await connectDB()
         await ItemModel.updateOne( {_id: req.query.id}, req.body)
--- utils/auth.js
+++ utils/auth.js
@@ -6,13 +6,14 @@ const auth = (handler) => {
         if(req.method === "GET"){
             return handler(req, res)
         }
-        const token = await req.headers.authorization.split("  ")[1]
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY0BnbWFpbC5jb20iLCJpYXQiOjE2NTI5NjkyMjMsImV4cCI6MTY1MzA1MjAyM30.iZkZ4WkvOAoLDE67W3rbgGW75k5ajf4Ot0eqPYWxQQk"
+        //await req.headers.authorization.split(" ")[1]
             if(!token){
                 return res.status(401).json({message: "トークンがありません"})
             }
             try{
                 const decoded = jwt.verify(token, secret_key)
-                console.log(decoded)
+                req.body.email = decoded.email
                 return handler(req, res)
             } catch(err){
                 return res.status(401).json({message: "トークンが正しくないので、ログインしてください"})
